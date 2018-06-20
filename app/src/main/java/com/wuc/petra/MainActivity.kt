6587a2e1c0960cf4577a9ce27c0b8352ee0a7dae
iChon <429215265@qq.com>
2018-06-20 16:26:32 +0800

    视图注入(反射版)--增加事件注入

--- app/src/main/java/com/wuc/petra/MainActivity.kt
+++ app/src/main/java/com/wuc/petra/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.wuc.petra
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.wuc.petra.viewinject.InjectTestReflectActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_inject_reflect.setOnClickListener {
+            startActivity<InjectTestReflectActivity>()
+        }
     }
+
 }
---
+++ app/src/main/java/com/wuc/petra/util/MD5.java
@@ -0,0 +1,39 @@
+package com.wuc.petra.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MD5 {
+
+    private static char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6',  '7', '8',
+            '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+    private static MessageDigest messagedigest = null;
+
+    static {
+        try {
+            messagedigest = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            System.err.println("MessageDigest初始化失败");
+            e.printStackTrace();
+        }
+    }
+
+    public static String getMD5String(byte[] bytes) {
+        messagedigest.update(bytes);
+        byte[] digest = messagedigest.digest();
+        return bufferToHex(digest, 0, digest.length);
+    }
+
+    private static String bufferToHex(byte bytes[], int m, int n) {
+        StringBuilder sb = new StringBuilder(2 * n);
+        int k = m + n;
+        for (int l = m; l < k; l++) {
+            char c0 = hexDigits[(bytes[l] & 0xf0) >> 4];
+            char c1 = hexDigits[bytes[l] & 0xf];
+            sb.append(c0);
+            sb.append(c1);
+        }
+        return sb.toString();
+    }
+
+}
